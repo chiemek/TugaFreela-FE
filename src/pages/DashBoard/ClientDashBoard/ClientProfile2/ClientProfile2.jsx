82e0727a398b1577f7fe8ab1f16d43acef9e45a9
chiemek <ekeson1085@gmail.com>
2024-08-28 11:02:42 +0100

    added axios too

--- src/pages/DashBoard/ClientDashBoard/ClientProfile2/ClientProfile2.jsx
+++ src/pages/DashBoard/ClientDashBoard/ClientProfile2/ClientProfile2.jsx
@@ -14,6 +14,7 @@ import Footer2 from "../../../../components/Footer/Footer2/Footer2";
 import { useNavigate } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 const ClientProfile2 = () => {
   const [user, setUser] = useState(null); // Initialize user state
